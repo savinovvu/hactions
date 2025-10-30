f077933b37e791fca1ecfd75797542aeb146122e
Vasiliy Savinov <savinov_vu@inbox.ru>
2025-10-30 20:41:07 +0800

    init

--- src/main/java/org/example/hactions/MainController.java
+++ src/main/java/org/example/hactions/MainController.java
@@ -1,10 +1,10 @@
 package org.example.hactions;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
     @GetMapping("/")
