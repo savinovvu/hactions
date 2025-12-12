c87ec0f2c670d604a40260cab25f721141c4c26a
Vasiliy Savinov <savinov_vu@inbox.ru>
2025-12-12 12:21:14 +0800

    docker

--- src/main/java/org/example/hactions/HactionsApplication.java
+++ src/main/java/org/example/hactions/HactionsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HactionsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HactionsApplication.class, args);
     }
-
 }
